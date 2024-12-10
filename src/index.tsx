f0a5f370298a5d88d510b724e08f1328fd41b9b7
Sara Jalal <sara.jalal.115@gmail.com>
2024-12-10 15:52:32 +0330

    feat: styles.css with root

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import TestPage from './TestPage';
-import ReactDOM from 'react-dom/client';
+import "./assets/styles.css";
+import TestPage from "./TestPage";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <TestPage />
+    <div id="modal-root" />
   </React.StrictMode>
 );
