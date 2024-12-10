e7ccb38b4fbfa71d3c82e13e56cc38d8fe7f80da
hamidrezahashemi71 <hh7192@gmail.com>
2024-12-10 18:25:17 +0330

    feat(drawer component)

---
+++ src/components/drawer/index.tsx
@@ -0,0 +1,72 @@
+import { Fragment, ReactNode } from "react";
+import { createPortal } from "react-dom";
+import styles from "./style.module.css";
+
+const Drawer = (props: DrawerProps & { children: ReactNode }) => {
+  const {
+    open,
+    openDuration = 300,
+    willBeClosed,
+    handleClose,
+    closeDuration = 200,
+    children,
+    sheetClassName = "",
+    backdropClassName = "",
+    direction = "bottom",
+  } = props;
+
+  const directionKeyframes = {
+    bottom: {
+      open: `slide-bottom-in`,
+      close: `slide-bottom-out`,
+    },
+    top: {
+      open: `slide-top-in`,
+      close: `slide-top-out`,
+    },
+    left: {
+      open: `slide-left-in`,
+      close: `slide-left-out`,
+    },
+    right: {
+      open: `slide-right-in`,
+      close: `slide-right-out`,
+    },
+  };
+
+  const animations = {
+    backdrop: {
+      false: `dialog-fade-in ${openDuration}ms`,
+      true: `dialog-fade-in ${closeDuration}ms`,
+    },
+    sheet: {
+      false: `${directionKeyframes[direction].open} ${openDuration}ms`,
+      true: `${directionKeyframes[direction].close} ${closeDuration}ms`,
+    },
+  };
+
+  if (open)
+    return createPortal(
+      <Fragment>
+        <div
+          onClick={handleClose}
+          className={`${styles.backdrop} ${backdropClassName}`}
+          style={{
+            animation: animations.backdrop[`${willBeClosed}`],
+          }}
+        />
+        <div
+          className={`${styles.sheet} ${sheetClassName} ${styles[direction]}`}
+          style={{
+            animation: animations.sheet[`${willBeClosed}`],
+          }}
+        >
+          {children}
+        </div>
+      </Fragment>,
+      document.getElementById("modal-root")!
+    );
+  return null;
+};
+
+export default Drawer;
