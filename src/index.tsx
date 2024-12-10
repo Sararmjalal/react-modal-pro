8cfbfb432bfd2e3f4eb3d9073e7b640ae6d74a64
Sara Jalal <sara.jalal.115@gmail.com>
2024-12-10 15:02:20 +0330

    updated: extra files removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import './index.css';
 import React from 'react';
-import App from './App';
+import TestPage from './TestPage';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <App />
+    <TestPage />
   </React.StrictMode>
 );
-
-reportWebVitals();
