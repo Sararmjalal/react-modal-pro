fa0fa29a4af740b1e9ecfca8bf570a1f0ee4ac19
hamidrezahashemi71 <hh7192@gmail.com>
2024-12-11 17:41:48 +0330

    wip(sidebar)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 import "./assets/styles.css";
 import TestPage from "./TestPage";
 import ReactDOM from "react-dom/client";
-import ModalProvider from './components/providers/ModalProvider';
+import ModalProvider from "./components/providers/ModalProvider";
+import SidebarTest from "./SidebarTest";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <ModalProvider>
       <TestPage />
+      <SidebarTest />
     </ModalProvider>
     <div id="modal-root" />
   </React.StrictMode>
