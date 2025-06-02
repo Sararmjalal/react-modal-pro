7d52abec2dd107c0112569266cb078ecf7d7cf60
Sararmjalal <sara.jalal.115@gmail.com>
2025-06-02 20:21:33 +0330

    updated: closing modal with queue

--- src/lib/utils/onClose.ts
+++ src/lib/utils/onClose.ts
@@ -2,15 +2,14 @@ import { onCloseProps } from "../types";
 
 export const onClose = ({ modals, closeDuration, closeCb, removeModal, key }: onCloseProps) => {
   const { open, willBeClosed, isRecentlyClosed } = modals[key] ?? { open: false, willBeClosed: false }
-  // console.log("key in close", key)
   if (open && willBeClosed && !isRecentlyClosed) {
     const currentState = window.history.state || {};
     const isAlreadyInState = currentState.modalStack ? currentState.modalStack.includes(key) : false
     if (isAlreadyInState) {
       const clone = { ...currentState }
-      const newStack = clone.modalStack.filter((item: string) => item !== key)
+      const modalIndex = clone.modalStack.findIndex((item: string) => item === key)
+      const newStack = clone.modalStack.slice(0, modalIndex)
       clone.modalStack = newStack
-      // console.log("clone in on close", { ...clone })
       window.history.replaceState({ ...clone }, '')
     }
     let timeout;
