b38cca39dbd38b4ac84e88159a513c475bd0ce20
Sararmjalal <sara.jalal.115@gmail.com>
2025-06-11 17:09:29 +0330

    updated: updateCloseCb out of timeout

--- src/lib/utils/onClose.ts
+++ src/lib/utils/onClose.ts
@@ -3,6 +3,7 @@ import { onCloseProps } from "../types";
 export const onClose = ({ thisModal, closeDuration, removeModal, key, updateCloseCb }: onCloseProps) => {
   const { open, willBeClosed, isRecentlyClosed } = thisModal
   if (open && willBeClosed && !isRecentlyClosed) {
+    updateCloseCb()
     const currentState = window.history.state || {};
     const isAlreadyInState = currentState.modalStack ? currentState.modalStack.includes(key) : false
     if (isAlreadyInState) {
@@ -17,10 +18,5 @@ export const onClose = ({ thisModal, closeDuration, removeModal, key, updateClos
     timeout = setTimeout(() => {
       removeModal(key);
     }, closeDuration - 50);
-    let timeout2;
-    if (timeout2) timeout = undefined;
-    timeout2 = setTimeout(() => {
-      updateCloseCb()
-    }, closeDuration);
   }
 }
