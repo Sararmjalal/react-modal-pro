90f13fedf9c411f3e0a180e462ea44cce8357b2c
Sararmjalal <sara.jalal.115@gmail.com>
2025-07-05 17:47:49 +0330

    updated: global styling + scroll to top removed

--- src/lib/hooks/usePreventBgScroll.ts
+++ src/lib/hooks/usePreventBgScroll.ts
@@ -1,15 +1,14 @@
 import { useEffect } from "react";
 
 export const usePreventBgScroll = (open: boolean) => {
-    useEffect(() => {
-        if (open) {
-            window.scrollTo(0, 0);
-            document.body.style.overflow = "hidden";
-            document.body.style.overscrollBehavior = "none";
-            return () => {
-                document.body.style.overflow = "auto";
-                document.body.style.overscrollBehavior = "initial";
-            };
-        }
-    }, [open]);
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+      document.body.style.overscrollBehavior = "none";
+      return () => {
+        document.body.style.overflow = "auto";
+        document.body.style.overscrollBehavior = "initial";
+      };
+    }
+  }, [open]);
 }
