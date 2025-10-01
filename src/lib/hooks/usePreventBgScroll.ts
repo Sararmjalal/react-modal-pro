408d2ad2dd5ce5b0ce381542037b2b8e2881363f
Sararmjalal <sara.jalal.115@gmail.com>
2025-10-01 18:10:32 +0330

    fix: backdrop styles

--- src/lib/hooks/usePreventBgScroll.ts
+++ src/lib/hooks/usePreventBgScroll.ts
@@ -1,11 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export const usePreventBgScroll = (open: boolean) => {
+  const scrollRef = useRef(0)
   useEffect(() => {
     if (open) {
+      scrollRef.current = document.body.scrollTop || window.scrollY
+      window.scrollTo({ top: scrollRef.current - 56 })
       document.body.style.overflow = "hidden";
       document.body.style.overscrollBehavior = "none";
       return () => {
+        window.scrollTo({ top: scrollRef.current })
         document.body.style.overflow = "auto";
         document.body.style.overscrollBehavior = "initial";
       };
